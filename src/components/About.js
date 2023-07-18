bb0519116a590f22ff0a3099a592f0be3f3d5e61
Moses <mochengo29@gmail.com>
2023-07-18 12:50:58 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am an 18 year old web developer based in Nairobi, Kenya</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
